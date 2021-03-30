8e9130a2a7daa51786a2676be5bfd917702f3de6
Developer-felix <onjombafelix1@gmail.com>
2021-03-29 22:02:20 -0700

    Server created

--- server.py
+++ server.py
@@ -9,3 +9,12 @@ s.bind("localhost",9999)
 s.listen(3)
 
 print("Waiting for conections")
+
+while True:
+    c, addr = s.accept()
+
+    print("Connected with",addr)
+
+    c.send("Welcome to codewithfelix")
+
+    c.close()
